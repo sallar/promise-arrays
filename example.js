c6bd72a99eb0f7e1c61997fb7d0d88854806ceb5
Sallar Kaboli <sallar.kaboli@gmail.com>
2015-09-07 23:21:08 +0300

    First implementation, example file

---
+++ example.js
@@ -0,0 +1,23 @@
+/**
+ * @author Sallar Kaboli <sallar.kaboli@gmail.com>
+ * @date 07.09.2015
+ * @TODO Remove this file
+ */
+var async = require('./async-array');
+
+// Test Array
+var nums = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// Map
+async.map(nums, function (num, i, resolve) {
+    resolve(num * 10);
+}).then(function (result) {
+    console.log(result);
+});
+
+// Filter
+async.filter(nums, function (num, i, resolve) {
+    resolve(num > 2 && num < 7);
+}).then(function (result) {
+    console.log(result);
+});
